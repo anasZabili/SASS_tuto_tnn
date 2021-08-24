51fd1e83e6c6ba0aa29af30e20aa2954aae90589
anas zabili <anaszab78@gmail.com>
2021-08-24 18:35:42 +0200

    <Feat> SASS + Glup setup

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+// create the css file from a scss source
+function buildStyles() {
+  return src("index.scss").pipe(sass()).pipe(dest("css"));
+}
+
+// watch our source file and run buildStyles when the source file changed
+function watchTask() {
+  // first argument the file to wath
+  // 2nd arg the callback to run when the files changed
+  watch(["index.scss"], buildStyles);
+}
+
+exports.default = series(buildStyles, watchTask);
