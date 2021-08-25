96b7c18670d97eec9d5b36dda1fa06fabf254fd2
anas zabili <anaszab78@gmail.com>
2021-08-25 16:08:22 +0200

    folder tree

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require("gulp-sass")(require("sass"));
 
 // create the css file from a scss source
 function buildStyles() {
-  return src("*.scss").pipe(sass()).pipe(dest("css"));
+  return src("shinobicsslib/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 // watch our source file and run buildStyles when the source file changed
 function watchTask() {
   // first argument the file to wath
   // 2nd arg the callback to run when the files changed
-  watch(["*.scss"], buildStyles);
+  watch(["shinobicsslib/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
